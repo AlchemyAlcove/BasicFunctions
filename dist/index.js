bce279daed5c84c8a6118b0ea290759e8d9fa770
Mike Hoffert <mike.hoffert@gmail.com>
2018-10-06 08:09:24 -0600

    isNil function

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.isNil = undefined;
+
+var _isNil = require("./isNil");
+
+var _isNil2 = _interopRequireDefault(_isNil);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.isNil = _isNil2.default;
